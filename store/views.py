09975423ac6f83ca0e43715d103f9be38785ea2e
Saleh6233 <salehsazzad6233@gmail.com>
2024-12-04 22:04:45 +0600

    Create initial model, template, url, views

---
+++ store/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+
+
+# Create your views here.
+
+
+def store(request):
+    all_products = Product.objects.all()
+    context = {'my_products': all_products}
+
+    return render(request, 'store/store.html', context)
+
+
+def categories(request):
+    all_categories = Category.objects.all()
+
+    return {'all_categories': all_categories}
+
+
+def product_info(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    context = {'product': product}
+
+    return render(request, 'store/product-info.html', context)
