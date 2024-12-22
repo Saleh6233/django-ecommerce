fff69fe2686b5e112158fde3c4ae04cc70c24646
Saleh6233 <salehsazzad6233@gmail.com>
2024-12-23 00:55:28 +0600

    finish cart add functionality

--- cart/cart.py
+++ cart/cart.py
@@ -1,15 +1,64 @@
+from decimal import Decimal
+from store.models import Product
+
+
 class Cart():
 
     def __init__(self, request):
-
         self.session = request.session
 
         cart = self.session.get('session_key')
 
         # New user - generate a new session
         if 'session_key' not in request.session:
-
             cart = self.session['session_key'] = {}
 
-
         self.cart = cart
+
+    def add(self, product, product_qty):
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            self.cart[product_id]['qty'] = product_qty
+
+        else:
+            self.cart[product_id] = {'price': str(product.price),
+                                     'qty': product_qty}
+
+        self.session.modified = True
+
+    def __len__(self):
+
+        return sum(item['qty'] for item in self.cart.values())
+
+    def __iter__(self):
+
+        all_product_ids = self.cart.keys()
+
+        products = Product.objects.filter(id__in=all_product_ids)
+
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+
+            item['total'] = item['price'] * item['qty']
+
+            yield item
+
+    def get_total(self):
+
+        total = 0  # Initialize total to 0
+
+        # Iterate over each item in the cart
+        for item in self.cart.values():
+            # Convert price to Decimal and multiply by quantity to get the item's total price
+            item_total = Decimal(item['price']) * item['qty']
+
+            # Add the item total to the running total
+            total += item_total
+
+        return total  # Return the final total
--- cart/views.py
+++ cart/views.py
@@ -1,17 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from store.models import Product
+from .cart import Cart
+from django.http import JsonResponse
 
 
 # Create your views here.
 
 
 def cart_summary(request):
+    cart = Cart(request)
 
-
-    return render(request, 'cart/cart-summary.html')
+    return render(request, 'cart/cart-summary.html', {'cart': cart})
 
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
+
+        product = get_object_or_404(Product, id=product_id)
+
+        cart.add(product=product, product_qty=product_quantity)
+
+        cart_quantity = cart.__len__()
+
+        response = JsonResponse({'qty': cart_quantity})
+        return response
 
 
 def cart_delete(request):
@@ -20,6 +36,3 @@ def cart_delete(request):
 
 def cart_update(request):
     pass
-
-
-
